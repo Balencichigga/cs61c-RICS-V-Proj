3d42eeccf7c5fc7c94e4cf797db489e2a859cf03
Balencichigga <123042067+Balencichigga@users.noreply.github.com>
2024-03-04 14:57:33 -0800

    Add files via upload

---
+++ tests/integration-custom/in/R-type.s
@@ -0,0 +1,17 @@
+li x1, 10         # Load immediate 10 into register x1
+li x2, 20         # Load immediate 20 into register x2
+
+add t0, t1, t2
+sub t0, t1, t2
+and t0, t1, t2
+or  t0, t1, t2
+xor t0, t1, t2
+sll t0, t1, t2
+srl t0, t1, t2
+sra t0, t1, t2
+slt t0, t1, t2
+mul t0, t1, t2
+mulh t0, t1, t2
+mulhu t0, t1, t2
+
+
